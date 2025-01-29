3d58733fbf37e0fd56157475d1494e166f180e51
DDX <ddxman@mail.ru>
2025-01-30 01:28:46 +0300

    Я ХЗ. Линк я вставил. Но это как то убого. Надо навернео передавать как то это на фронт и оформлять там. Но я пока хз как

--- app/Http/Controllers/miskaController.php
+++ app/Http/Controllers/miskaController.php
@@ -13,7 +13,7 @@ public function view(){
     $users = DB::table('sites')->where('sites_categorie_id', 'Миски')->get();
 
     foreach ($users as $user) {
-        echo $user->name."<br>";
+        echo "<a href=".$user->URL.">"."<button>".$user->name."</button></a><br>";
     }
         }
     }
